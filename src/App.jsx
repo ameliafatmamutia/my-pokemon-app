2c17a01e38e3ea9db5c9cd7d755a47eff05222cc
ameliafatmamutia <ameliafatmam@gmail.com>
2023-08-07 21:05:24 +0700

    install library

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import { BeakerIcon } from '@heroicons/react/24/solid'
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <BeakerIcon className="h-6 w-6 text-blue-500" />
+    </div>
+  );
+}
+
+export default App;
